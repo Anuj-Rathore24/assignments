be0258c419cd5b7c948d881c4c9a7bc19ac2ef14
Anuj-Rathore24 <anujsingh2409@gmail.com>
2022-05-08 20:50:05 +0530

    a

--- PP/assignment3.c
+++ PP/assignment3.c
@@ -1,86 +1,96 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
-#define n 2000
+#define n 1000
 
-int swap(long int x, long int y)
+long long int Partition(long long int arr[],long long int start,long long int end)
 {
-    long int temp = x;
-    x = y;
-    y = x;
+	long long int p_index=start;
+	long long int shift_index=start;
+	
+	for(long long int i=start+1;i<=end;i++)
+	{
+		if(arr[i] < arr[p_index])
+		{
+			long long int temp = arr[i];
+			long long int set = p_index;
+			while(shift_index!=i)
+			{	
+				for(long long int j=shift_index;j>=p_index;j--)
+				{
+					arr[j+1] = arr[j];	
+				}
+				p_index++;
+				shift_index++;
+			}
+			arr[set] = temp;
+		}
+		
+		else
+		{
+			shift_index++;
+		}
+	}
+	
+	
+	return p_index;
 }
 
-int partition(long int arr[100], long int low, long int high)
+void Quick_sort_p(long long int arr[],long long int start,long long int end)
 {
-    long int pivot = arr[high];
-    long int temp, j, i = -1;
-    for (j = low; j < high; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            long int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            swap(arr[i], arr[j]);
-            return i;
-        }
-        swap(arr[++i], arr[j]);
-            long int temp=arr[i++];
-            arr[i++]=arr[j];
-            arr[j]=temp;
-        return i;
-    }
+	if(start<end)
+	{
+		long long int p_index = Partition(arr,start,end);
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{Quick_sort_p(arr,start,p_index-1);}
+			
+			#pragma omp section
+			{Quick_sort_p(arr,p_index+1,end);}
+		}
+	}
 }
 
-    // parallel
-    long int quicksort_p(long int arr[], long int low, long int high)
-    {
-        if (low < high)
-        {
-            long int mid = partition(arr, low, high);
 
-            #pragma omp parallel sections
-            {
-                #pragma omp section
-                {
-                    quicksort_p(arr, low, mid - 1);
-                }
-                #pragma omp section
-                {
-                    quicksort_p(arr, mid + 1, high);
-                }
-            }
-        }
-    }
-    // serial
-    long int quicksort_s(long int arr[], long int low, long int high)
-    {
-        if (low < high)
-        {
-            long int mid = partition(arr, low, high);
-            quicksort_s(arr, low, mid - 1);
-            quicksort_s(arr, mid + 1, high);
-        }
-    }
+void Quick_sort_s(long long int arr[],long long int start,long long int end)
+{
+	if(start<end)
+	{
+		long long int p_index = Partition(arr,start,end);
+		
+		Quick_sort_s(arr,start,p_index-1);
+		Quick_sort_s(arr,p_index+1,end);
+	}
+}
 
-    int main()
-    {
-        long int arr1[n], arr2[n];
-        for (long int i = 0; i < n; i++)
-        {
-            arr1[i] = arr2[i] = rand() % n;
-        }
 
-        double start = omp_get_wtime();
-        quicksort_s(arr1, 0, n - 1);
-        double stop = omp_get_wtime();
-        printf("\nSerial Quicksort time= %f", stop - start);
 
-        start = omp_get_wtime();
-        quicksort_p(arr2, 0, n - 1);
-        stop = omp_get_wtime();
-        printf("\nParallel Quicksort time+ %f", stop - start);
-
-        return 0;
-    }
+int main()
+{
+	omp_set_nested(1);
+	long long int arr1[n];
+	long long int arr2[n];
+	
+	for(long long int i=0;i<n;i++)
+	{
+		arr2[i] = arr1[i] = rand()%n;
+	}
+	
+	double start_s = omp_get_wtime();
+	Quick_sort_s(arr1,0,n-1);
+	double stop_s = omp_get_wtime();
+	
+	printf("\nSerial Time: %lf",stop_s-start_s);
+	
+	printf("\n");
+	
+	
+	double start_p = omp_get_wtime();
+	Quick_sort_p(arr2,0,n-1);
+	double stop_p = omp_get_wtime();
+	
+	printf("\n\nParallel Time: %lf",stop_p-start_p);
+	
+	return 0;
+}
