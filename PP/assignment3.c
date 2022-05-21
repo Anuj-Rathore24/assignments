49696a48407910a56792d4e2b2b7f3b8f7b6a8bb
Anuj-Rathore24 <anujsingh2409@gmail.com>
2022-05-21 12:05:26 +0530

    starting jsp

--- PP/assignment3.c
+++ PP/assignment3.c
@@ -1,96 +1,76 @@
-#include <stdio.h>
-#include <omp.h>
 #include <stdlib.h>
-#define n 1000
-
-long long int Partition(long long int arr[],long long int start,long long int end)
+#include <stdio.h>
+#define max 4000
+#include <omp.h>
+int partition(int arr[], int low, int high)
 {
-	long long int p_index=start;
-	long long int shift_index=start;
-	
-	for(long long int i=start+1;i<=end;i++)
+	int pivot;
+	pivot = arr[high];
+	int i = low - 1;
+	int j;
+	for (j = low; j < high; j++)
 	{
-		if(arr[i] < arr[p_index])
+		if (arr[j] < pivot)
 		{
-			long long int temp = arr[i];
-			long long int set = p_index;
-			while(shift_index!=i)
-			{	
-				for(long long int j=shift_index;j>=p_index;j--)
-				{
-					arr[j+1] = arr[j];	
-				}
-				p_index++;
-				shift_index++;
+			i++;
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	i++;
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+	return i;
+}
+void quicksort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int p = partition(arr, low, high);
+		quicksort(arr, low, p - 1);
+		quicksort(arr, p + 1, high);
+	}
+}
+void quicksortparallel(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int p = partition(arr, low, high);
+#pragma omp parallel sections private(low, high)
+		{
+#pragma omp section
+			{
+				quicksort(arr, low, p - 1);
 			}
-			arr[set] = temp;
-		}
-		
-		else
-		{
-			shift_index++;
-		}
-	}
-	
-	
-	return p_index;
-}
-
-void Quick_sort_p(long long int arr[],long long int start,long long int end)
-{
-	if(start<end)
-	{
-		long long int p_index = Partition(arr,start,end);
-		#pragma omp parallel sections
-		{
-			#pragma omp section
-			{Quick_sort_p(arr,start,p_index-1);}
-			
-			#pragma omp section
-			{Quick_sort_p(arr,p_index+1,end);}
+#pragma omp section
+			{
+				quicksort(arr, p + 1, high);
+			}
 		}
 	}
 }
-
-
-void Quick_sort_s(long long int arr[],long long int start,long long int end)
-{
-	if(start<end)
-	{
-		long long int p_index = Partition(arr,start,end);
-		
-		Quick_sort_s(arr,start,p_index-1);
-		Quick_sort_s(arr,p_index+1,end);
-	}
-}
-
-
-
 int main()
 {
-	omp_set_nested(1);
-	long long int arr1[n];
-	long long int arr2[n];
-	
-	for(long long int i=0;i<n;i++)
+	int i, arrS[max], arrP[max];
+	for (i = 0; i < max; i++)
 	{
-		arr2[i] = arr1[i] = rand()%n;
+		arrS[i] = arrP[i] = rand() % max;
 	}
+	printf("\nSerial:");
+	double start = omp_get_wtime();
+	quicksort(arrS, 0, max - 1);
+	double stop = omp_get_wtime();
+	printf("Serial Execution time:%f ", stop - start);
 	
-	double start_s = omp_get_wtime();
-	Quick_sort_s(arr1,0,n-1);
-	double stop_s = omp_get_wtime();
-	
-	printf("\nSerial Time: %lf",stop_s-start_s);
-	
+
 	printf("\n");
-	
-	
-	double start_p = omp_get_wtime();
-	Quick_sort_p(arr2,0,n-1);
-	double stop_p = omp_get_wtime();
-	
-	printf("\n\nParallel Time: %lf",stop_p-start_p);
+	printf("\nParallel:");
+	double start1 = omp_get_wtime();
+	quicksort(arrP, 0, max - 1);
+	double stop1 = omp_get_wtime();
+	printf("Parallel Execution time:%f ", stop1 - start1);
 	
 	return 0;
 }
---
+++ java_programming/assignment12_part1.java
@@ -0,0 +1,173 @@
+import java.sql.*;
+import java.util.*;
+
+public class assignment12_part1 {
+
+    /******************************************************
+ *Assignment: To implement JDBC.
+ *Name,RollNo: Pooja Baba,TE 302003.
+ ******************************************************/
+
+
+	// private static final Readable InputStream = null;
+	static  String URL="jdbc:mysql://localhost/assignment";	//final constant optional
+	/* jdbc-protocol that is used
+	 * mysql-subprotocol
+	 * 172.20.35.20-hostname
+	 */
+	static  String USER="root";
+	static String PASWD="Philanthropist$420";
+	public static void main(String args[])
+	{
+		int ch,ans;
+		try
+		{
+			do
+			{
+					//1.Get a connection to the database.
+					Connection con=DriverManager.getConnection(URL,USER,PASWD);
+					/*getConnection-method which will return an object*/
+				
+					System.out.println("Connection accquired!!");
+						
+					//2.Create a statement.
+					Statement stat=con.createStatement();
+					System.out.println("***MENU***\n1.Insert a new data.\n2.Display data.\n3.Delete data.\n4.Update data.\n5.Search a value.\nEnter your choice: ");
+					Scanner s=new Scanner(System.in);
+					ch=s.nextInt();
+					/***************************INSERT A NEW DATA IN STUDENTS TABLE**********************************************/		
+					if(ch==1)
+					{
+						Scanner scan=new Scanner(System.in);
+						int roll;
+						String name,add;
+						System.out.print("Enter the name: ");
+						name=scan.next();
+						System.out.print("Enter the roll number: ");
+						roll=scan.nextInt();
+						System.out.print("Enter the address: ");
+						add=scan.next();
+						stat.executeUpdate("insert into Student values("+roll+",'"+name+"','"+add+"');");		
+						//above statement returns 1 if the required item is found and it is deleted.Else it is 0
+						ResultSet r=stat.executeQuery("select *from Student;");
+						System.out.print("\tRoll_no\t\t\tName\t\tAddress\n");
+						int i = 0;
+						while(r.next())
+						{
+							String rn=r.getString("Rollno");
+							String n=r.getString("Name");
+							String a=r.getString("Address");
+							System.out.println("\t"+rn+"\t\t\t"+n+"\t\t\t"+a);
+							i+=1;
+						}
+						System.out.println("Record inserted!!");
+						con.close();
+						scan.close();
+					}
+					/***************************SELECT FROM STUDENT TABLE**********************************************/
+					if(ch==2)
+					{
+						//3.Execute the statement with SQL Query.
+						ResultSet res=stat.executeQuery("select *from Student;");
+						/*ResultSet contains the table for the fired Query.
+						 * executeQuery-will execute the select queries.
+						 * executeUpdate-will execute insert,delete,etc  
+						 */
+						System.out.print("\tRollNo\t\tName\t\tAddress\n");
+						while(res.next())
+						{
+							String rn=res.getString("Rollno");
+							String name=res.getString("Name");
+							String add=res.getString("Address");
+							System.out.println("\t"+rn+"\t\t"+name+"\t\t"+add);
+						}	
+					}
+					/***************************DELETE FROM RESULT TABLE**********************************************/
+						if(ch==3)
+						{
+		
+							stat.executeUpdate("delete from Result where Roll_no=5 and Sub_code=101;");		
+							//above statement returns 1 if the required item is found and it is deleted.Else it is 0
+							ResultSet r=stat.executeQuery("select *from Result;");
+							System.out.print("\tRoll_no\t\t\tSub_code\t\tMarks\n");
+							int i = 0;
+							while(r.next())
+							{
+								String rn=r.getString("Roll_no");
+								String sc=r.getString("Sub_code");
+								String m=r.getString("Marks");
+								System.out.println("\t"+rn+"\t\t\t"+sc+"\t\t\t"+m);
+								i+=1;
+							}
+							System.out.println("Record deleted!!");
+							con.close();
+						}
+						/***************************UPDATE A VALUE FROM RESULT TABLE**********************************************/
+						if(ch==4)
+						{
+		
+							Scanner sca=new Scanner(System.in);
+							int marks,rol,subc;
+							System.out.print("Enter roll number: ");
+							rol=sca.nextInt();
+							System.out.print("Enter new subject code: ");
+							subc=sca.nextInt();
+							System.out.print("Enter new marks: ");
+							marks=sca.nextInt();		
+							stat.executeUpdate("update  Result set Marks="+marks+" where Roll_no="+rol+" and Sub_code="+subc+";");		
+							//above statement returns 1 if the required item is found and it is deleted.Else it is 0
+							ResultSet r=stat.executeQuery("select *from Result;");
+							System.out.print("\tRoll_no\t\t\tSub_code\t\tMarks\n");
+							int i = 0;
+							while(r.next())
+							{
+								String rn=r.getString("Roll_no");
+								String sc=r.getString("Sub_code");
+								String m=r.getString("Marks");
+								System.out.println("\t"+rn+"\t\t\t"+sc+"\t\t\t"+m);
+								i+=1;
+							}
+							System.out.println("Record updated!!");
+							con.close();
+						}
+						/***************************SEARCH A VALUE***********************************/				
+						if(ch==5)
+						{					
+							CallableStatement stmnt =null;
+							Scanner sca=new Scanner(System.in);
+							int rolno;
+							System.out.print("Enter roll number: ");
+							rolno=sca.nextInt();
+							String sql="{call fetch_detail(?,?,?)}";
+							stmnt=con.prepareCall(sql);
+							 //Bind IN parameter first, then bind OUT parameter
+							stmnt.setInt(1,rolno);// This would set rollno
+							// Because second parameter is OUT so register it
+						      stmnt.registerOutParameter(2,java.sql.Types.VARCHAR);
+						      stmnt.registerOutParameter(3,java.sql.Types.VARCHAR);
+							//above statement returns 1 if the required item is found and it is deleted.Else it is 0
+							
+						    //Use execute method to run stored procedure.
+						      stmnt.execute();
+						      
+						    //Retrieve employee name with getXXX method
+						      String SName = stmnt.getString(2);
+						      String SAddress=stmnt.getString(3);
+						      System.out.print("Student's Name: "+SName+"\nStudent's Address: "+SAddress);      
+						      stmnt.close();
+						      con.close();
+							  sca.close();
+						}
+						System.out.print("Do you want to continue: ");
+						ans=s.nextInt();
+			}while(ans==1);
+		}
+		catch (SQLException se){se.printStackTrace();} //SQL Exception 		
+	}
+	private static int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
+
+}
+    
+}
